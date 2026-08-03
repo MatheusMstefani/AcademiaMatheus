c2eed03b060637d673b3d40960b71c69eee48b07
Matheus <enzoymatheus@gmail.com>
2026-08-03 13:24:03 -0300

    adicionando as entidades

---
+++ AcademiaDoZe.Domain/Entities/AcessoColaborador.cs
@@ -0,0 +1,20 @@
+// Matheus Marques Stefani
+namespace AcademiaDoZe.Domain.Entities;
+
+public class AcessoColaborador : Entity
+{
+    public Colaborador Colaborador { get; private set; }
+    public DateTime DataHoraEntrada { get; private set; }
+    public DateTime? DataHoraSaida { get; private set; }
+
+    public AcessoColaborador(
+        int id,
+        Colaborador colaborador,
+        DateTime dataHoraEntrada,
+        DateTime? dataHoraSaida) : base(id)
+    {
+        Colaborador = colaborador;
+        DataHoraEntrada = dataHoraEntrada;
+        DataHoraSaida = dataHoraSaida;
+    }
+}
